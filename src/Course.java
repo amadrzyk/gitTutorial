6827543a162abba3b4453a11e5fa716c44e18f65
amadrzyk <alexmadrzyk@gmail.com>
2017-02-14 14:56:25 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alexmadrzyk on 2/14/17.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
